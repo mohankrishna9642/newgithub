29b56a066788e0b7e8440c934babc58c4ddeeb8c
mohankrishna <mohankrishna5249@gmail.com>
2024-10-28 09:30:00 +0530

    firstcopmmit

---
+++ src/adding/Sum.java
@@ -0,0 +1,16 @@
+package adding;
+
+public class Sum {
+	public void add()
+	{
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println(c);
+	}
+	public static void main(String[] args) {
+		Sum bb=new Sum();
+		bb.add();
+	}
+
+}
